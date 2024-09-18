837f44e677bcad0a3446bed1fdf9d5a9dc6daa16
JuliaRodi <nekrasowayulia2016@gmail.com>
2024-09-18 23:42:30 +0300

    Add files via upload
    2023/09/23 00:00|Практическая работа по уроку "Организация программ и методы строк".

---
+++ module_1_4.py
@@ -0,0 +1,10 @@
+my_string = input("Ваш текст: ")
+number_of_characters = len(my_string)
+print("Количество символов введённого текста: ",number_of_characters)
+my_string = "Практическое задание"
+print(my_string. upper())
+print(my_string. lower())
+print(my_string[0])
+print(my_string[-1])
+my_string = my_string.replace(" ", "")
+print(my_string)
