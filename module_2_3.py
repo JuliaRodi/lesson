73817e90a71f08302cb71d8efbcf18379395fe60
JuliaRodi <nekrasowayulia2016@gmail.com>
2024-09-27 23:13:33 +0300

    Delete module_2_3.py

--- module_2_3.py
+++
@@ -1,9 +0,0 @@
-my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
-i = 0
-(len(my_list))
-while i < len(my_list):
-    if my_list[i] >= 0:
-        print(my_list[i])
-        i += 1 #i=1+1
-    else:
-        break
