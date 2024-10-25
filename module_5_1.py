d0192931e782f81e79a673457157b7d6aaf5f352
JuliaRodi <nekrasowayulia2016@gmail.com>
2024-10-26 01:13:51 +0300

    module_5_2.py
    Домашняя работа по уроку "Специальные методы классов". Продолжила работу в module_5_1

--- module_5_1.py
+++ module_5_1.py
@@ -2,7 +2,6 @@ class House:
     def __init__ (self, name, number_of_floors):
         self.name = name
         self.number_of_floors = int(number_of_floors)
-
     def go_to(self, new_floor):
         int(new_floor)
         if new_floor > self.number_of_floors or new_floor < 1:
@@ -10,6 +9,10 @@ class House:
         else:
             for i in range(1, new_floor + 1):
                 print(f' "{self.name}", вы находитесь на {i} этаже')
+    def __len__ (self):
+        return self.number_of_floors
+    def __str__(self):
+        return f'"Название: "{self.name}", кол-во этажей: {self.number_of_floors}"'
 
 h1 = House('ЖК Горский', 18)
 h2 = House('Домик в деревне', 2)
@@ -19,3 +22,12 @@ h1.go_to(5)
 h2.go_to(10)
 h3.go_to(17)
 h2.go_to(1)
+
+print(len(h1))
+print(len(h2))
+print(len(h3))
+
+print(str(h1))
+print(str(h2))
+print(str(h3))
+
