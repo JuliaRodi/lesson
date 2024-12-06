7efd1e4eecd0b95d5bee46255a53ae0a502c6c53
JuliaRodi <nekrasowayulia2016@gmail.com>
2024-12-07 00:12:41 +0300

    "Генераторы"
    Домашнее задание по теме "Генераторы"

---
+++ module_9_6.py
@@ -0,0 +1,15 @@
+from itertools import combinations
+
+def all_variants(text):
+    list_1 = []
+    for i in range(1, len(text) + 1):
+        list_1 = []
+        b = combinations(text, i)
+        list_1.extend(b)
+        for j in list_1:
+            j = list(j)
+            yield (''.join(j))
+
+a = all_variants("abc")
+for i in a:
+    print(i)
